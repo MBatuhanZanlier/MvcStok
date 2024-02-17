4aae7f2b1a4773fcb7e9fc2cecb001f3b8f050d9
pc <pc@DESKTOP-A2G4J05>
2024-02-17 15:46:31 +0300

    Add project files.

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models;
+using MvcStok.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index(int sayfa=1)
+        {   
+            var degerler = db.TBLKATEGORI.ToList().ToPagedList(sayfa, 2);
+            //var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet] 
+        public  ActionResult YeniKategori()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKATEGORI p)
+        { 
+            if(!ModelState.IsValid)
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return View(); 
+        } 
+        public ActionResult Sil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+        public ActionResult Güncelle(int id)
+        {
+            var ktgr = db.TBLKATEGORI.Find(id);
+            return View("Güncelle",ktgr);
+        } 
+
+        public ActionResult GüncelleIki(TBLKATEGORI p) 
+        {
+            var ktg = db.TBLKATEGORI.Find(p.KATEGORIID);
+            ktg.KATEGORIAD = p.KATEGORIAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MvcStok.Models.Entity;
+
+using System.Data.Entity.Core.EntityClient;
+using System.Data.Entity.Core.Objects.DataClasses;
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        // GET: Urun
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORI.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p)
+        {
+            var ktg = db.TBLKATEGORI.Where(m => m.KATEGORIID == p.TBLKATEGORI.KATEGORIID).FirstOrDefault();
+            p.TBLKATEGORI = ktg;
+            db.TBLURUNLER.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Sil(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            db.TBLURUNLER.Remove(urun);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORI.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            var urun = db.TBLURUNLER.Find(id);
+            return View("UrunGetir", urun);
+        } 
+        public ActionResult Güncelle(TBLURUNLER p)
+        {
+            var urun = db.TBLURUNLER.Find(p.URUNID);
+            urun.URUNAD = p.URUNAD;
+            urun.MARKA = p.MARKA;
+            urun.STOK = p.STOK;
+            urun.FIYAT = p.FIYAT;
+            var ktg = db.TBLKATEGORI.Where(m => m.KATEGORIID == p.TBLKATEGORI.KATEGORIID).FirstOrDefault();
+            urun.URUNKATEGORI = ktg.KATEGORIID;
+            //urun.URUNKATEGORI = p.URUNKATEGORI; 
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
